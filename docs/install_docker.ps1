4d62e2ba3e7591e0fbf0f6e59410f3bb9e47f400
Alex de Sousa <alex@thebroken.link>
2026-06-09 18:44:16 +0200

    :bug: Fix version for PS1 install script

--- docs/install_docker.ps1
+++ docs/install_docker.ps1
@@ -17,7 +17,7 @@ param(
 $ErrorActionPreference = "Stop"
 
 $Repo        = "alexdesousa/planck"
-$Version     = "0.1.8"
+$Version     = "0.1.11"
 $PlanckHome  = Join-Path $HOME "planck"
 $ComposeUrl  = "https://raw.githubusercontent.com/$Repo/v$Version/planck_docker/compose.yml"
 $ComposeFile = Join-Path $PlanckHome "compose.yml"
